863485e9daefcd07db5173ccf63f5f2dcdb87398
cereaIx <alicepopilliu@gmail.com>
2025-01-22 13:23:21 -0500

    try fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/alicexliu.github.io/",
+  base: "/",
   css: {
     modules: {
       localsConvention: "camelCase", 
