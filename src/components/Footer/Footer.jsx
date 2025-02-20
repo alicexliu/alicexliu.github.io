8840edecc777cae8d036b1f066f6a072ca69b92a
cereaIx <alicepopilliu@gmail.com>
2025-02-19 22:14:46 -0500

    added demo reel

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import styles from "./Footer.module.css";
 
 export const Footer = () => {
-    return (
-        <div>Footer</div>
-    )
-}
+  return (
+    <footer className={styles.container}>
+      <div className={styles.content}>
+        <p>© 2025 Alice Liu</p>
+        <p>Made with <span className={styles.heart}>♥</span> by Alice Liu</p>
+      </div>
+    </footer>
+  );
+};
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,6 +8,20 @@ import { ProjectCard } from "./ProjectCard";
 export const Projects = () => {
     return (
       <section className={styles.container}>
+            {/* Demo Reel */}
+            <div className={styles.demoReel}>
+            <h2 className={styles.title}>Demo Reel</h2>
+                <div className={styles.videoContainer}>
+                    <iframe 
+                        src="https://player.vimeo.com/video/1052809432?h=1a55888763&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" 
+                        frameBorder="0" 
+                        allow="autoplay; fullscreen; picture-in-picture" 
+                        allowFullScreen
+                    ></iframe>
+                </div>
+            </div>
+           
+            {/* Projects List */}
             <h2 className={styles.title}>Projects</h2>
             <div className={styles.projects}> 
                 {projects.map((project, id) => {
